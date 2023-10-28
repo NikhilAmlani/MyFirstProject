235a36dc1c017a652d900c30960da31b90fa4646
Nikhil Amlani <nikhilamlani4@gmail.com>
2023-10-28 14:19:57 +0100

    my first project

--- MySecondClass.java
+++ MySecondClass.java
@@ -1,7 +1,10 @@
 package myfirstpackage;
 
 public class MySecondClass {
-    public static void main(String[] args)
-    {System.out
-    }.
-}
+    public static void main(String[] args) {
+        System.out.println("hello gujarati");
+    }
+
+
+    }
+
--- Myfirstclass.java
+++ Myfirstclass.java
@@ -1,11 +1,14 @@
 package myfirstpackage;
 
 public class Myfirstclass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 
 
 
     }
 
-    }
+
 
 
