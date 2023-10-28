2fedf48f97afa728b7da1e4df5565ed401128577
Nikhil Amlani <nikhilamlani4@gmail.com>
2023-10-28 12:24:12 +0100

    this is my first project

---
+++ MySecondClass.java
@@ -0,0 +1,7 @@
+package myfirstpackage;
+
+public class MySecondClass {
+    public static void main(String[] args)
+    {System.out
+    }.
+}
---
+++ Myfirstclass.java
@@ -0,0 +1,11 @@
+package myfirstpackage;
+
+public class Myfirstclass {
+
+
+
+    }
+
+    }
+
+
