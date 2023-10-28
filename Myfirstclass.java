a272209f135d1af2bc12c7ac571fabcb71e5d265
Nikhil Amlani <nikhilamlani4@gmail.com>
2023-10-28 14:51:38 +0100

    my first project

--- Myfirstclass.java
+++ Myfirstclass.java
@@ -3,7 +3,8 @@ package myfirstpackage;
 public class Myfirstclass {
     public static void main(String[] args) {
         System.out.println("hello world");
-        //hello america 
+        //hello india
+        
     }
 
 
